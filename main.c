cebf3b0f791c38e18fe7d9986d49dcd41e2e1de5
J Sravan Kumar <36272815+sravan888@users.noreply.github.com>
2018-06-08 08:48:57 -0700

    scholarship prorgram

---
+++ main.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+void main()
+
+{
+    
+int a,b,c,total,n;
+    
+printf("enter the three subjects marks\n");
+
+    scanf("%d%d%d",&a,&b,&c);
+   
+ if ((a>=35)&&(b>=35)&&(c>=35))
+  
+  printf("pass\n");
+   
+ else
+    
+printf("fail\n");
+  
+  {
+     
+   printf("enter the percentage\n");
+     
+   scanf("%d",&n);
+       
+   if(n>=80)
+ {
+     
+   printf("you got 80 percentage\n");
+   
+      }
+      
+  else{
+    
+    printf("you got less than 80 percentage\n");
+      
+  }
+  
+  {
+       
+ total=a+b+c;
+      
+
+  if(total>=260)
+       
+ {
+
+            printf("you got scholership\n");
+   
+     }
+
+        else{
+
+            printf(" you won't get scholership\n");
+       
+ }
+
+ }
+
+  
+}
+
+}
